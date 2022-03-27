e2104c7d7b1098c7739793b10025b6242f5b6785
Kübra Yiğiter <88934309+kyigiter@users.noreply.github.com>
2022-03-27 19:00:55 +0300

    Create 12_any_all.sql

---
+++ 12_any_all.sql
@@ -0,0 +1,24 @@
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--1--film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+select count(title) from film 
+where length >
+(select avg(length) from film);
+
+--2--film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+select count(title) from film 
+where rental_rate
+(select max(rental_rate) from film);
+
+--3--film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+select title from film 
+where rental_rate = any
+(select min(rental_rate) from film)
+and replacement_cost = any
+(select min(replacement_cost) from film);
+
+--4--payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+select distinct customer_id, count(rental_id) from payment 
+group by customer_id
+order by 2 desc
+limit 1;
